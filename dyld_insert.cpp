d6118f6bc2741cd533a60699c0277b766e3dda99
Tamas Foldi <tfoldi@starschema.net>
2016-12-03 14:59:05 +0100

    thread to look up qApp

--- dyld_insert.cpp
+++ dyld_insert.cpp
@@ -1,18 +1,40 @@
-#include <QApplication>
+#include <stdio.h>
+#include <unistd.h>
+#include <thread>
+#include <QCoreApplication>
 
+// shutdown flag for the service thread
+static bool shutdown = false;
+
+// standard thread handler
+std::thread * service_thread;
+
+// fwd decl
+void start_thread();
 
 // Initializer.
 __attribute__((constructor))
   static void initializer(void) {  
-    printf("[%s] initializer()\n", __FILE__);
+    printf("[%s] Starting service thread\n", __FILE__);
+    service_thread = new std::thread( start_thread );
   }
 
 // Finalizer.
 __attribute__((destructor))
   static void finalizer(void) {                               // 3
-    printf("[%s] finalizer()\n", __FILE__);
+    printf("[%s] stopping service thread()\n", __FILE__);
+    shutdown = true;
+    service_thread->join();
+    delete service_thread;
   }
 
-int foo() {
-  return 0;
+void start_thread() {
+  printf("thread started, looking for qApp\n");
+  
+  // search for QApplication
+  while ( !shutdown && !qApp ) 
+    usleep(250000);
+  
+
+  printf("got qApp = %p\n", qApp );
 }
