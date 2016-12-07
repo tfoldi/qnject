7cd0b3a7d472bf65efac39fd5a10e5a3c55c42f6
Tamas Foldi <tfoldi@starschema.net>
2016-12-07 14:35:50 +0100

    access all relevant qobject

--- vaccine/qobject.cpp
+++ vaccine/qobject.cpp
@@ -3,14 +3,20 @@
 #include "vaccine.h"
 
 #include <dlfcn.h>
+#include <map>
 #include <QApplication>
 #include <QList>
 #include <QObject>
 #include <QWidget>
+#include <QWindow>
 
 #include "../deps/json/json.hpp"
 
 namespace vaccine {
+
+#ifdef WITH_QT_STALKER
+  static std::map<std::string,void *> s_qobject_map;
+#endif
 
   void qobject_handler( 
       std::string & uri, 
@@ -18,15 +24,35 @@ namespace vaccine {
       void *ev_data,            
       struct http_message *hm)
   {
-    nlohmann::json j;
+    nlohmann::json resp;
     char buf[140];
 
     sprintf(buf,"qApp:%p",qApp);
 
-    j["qApp"] = buf;
-    j["appName"] = qPrintable(QApplication::applicationName());
+    resp["qApp"] = buf;
+    resp["appName"] = qPrintable(QApplication::applicationName());
+    
+    for( QObject * child : qApp->findChildren<QObject *>() )
+      resp["child"].push_back( qPrintable(child->objectName()) );
 
-    send_json(nc,j);
+    for( QObject * child : qApp->allWidgets() ) {
+      if (child && child->objectName() != "")  {
+        resp["widgets"].push_back( qPrintable(child->objectName()) );
+        if ( child->objectName() == "m_edtExplicitPassword" ) {
+          child->setProperty("text", QVariant("1234"));
+        }
+      }
+    }
+
+    for( QWindow * child : qApp->allWindows() ) {
+      resp["windows"].push_back( qPrintable(child->objectName()) );
+
+      for( QObject * obj : child->findChildren<QObject *>() )
+        resp["windows"][qPrintable(child->objectName())].push_back( qPrintable(obj->objectName()) );
+    }
+
+
+    send_json(nc,resp);
   }
 
   __attribute__((constructor))
@@ -37,11 +63,12 @@ namespace vaccine {
 
 }
 
+#ifdef WITH_QT_STALKER
 void QObject::setObjectName(const QString & name ) {
   typedef void (QObject::*f_setObjectName)(const QString & name);
   static f_setObjectName origMethod = 0;
 
-  printf("anyad: %s\n", qPrintable(name));
+  printf("Register object: %p %s\n", &name, qPrintable(name));
 
   if (origMethod == 0)
   {
@@ -58,16 +85,15 @@ void QObject::setObjectName(const QString & name ) {
     (this->*origMethod)(name);
 }
 
-#if 0
 QObject::~QObject() {
   typedef void (QObject::*f_destructor)();
   static f_destructor origMethod = 0;
 
-  printf("elment: %s\n", qPrintable(this->objectName()));
+  printf("elment: %s (%p)\n", qPrintable(this->objectName()), this);
 
   if (origMethod == 0)
   {
-    void *tmpPtr = dlsym(RTLD_NEXT, "_ZN7QObjectD1Ev");
+    void *tmpPtr = dlsym(RTLD_NEXT, "_ZN7QObjectD0Ev");
  
     if (tmpPtr)
       memcpy(&origMethod, &tmpPtr, sizeof(void *));
@@ -79,6 +105,6 @@ QObject::~QObject() {
   if (origMethod)
     (this->*origMethod)();
 }
-#endif
+#endif // WITH_QT_STALKER
 
 #endif // HAVE QT5CORE && QT5WIDGES
