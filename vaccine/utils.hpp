76d17c6a3f04448e7eb36f49f69641f7f984e96b
Tamas Foldi <tfoldi@starschema.net>
2016-12-27 14:04:24 +0100

    string utilities

---
+++ vaccine/utils.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "../deps/mongoose/mongoose.h"
+
+namespace vaccine {
+
+  std::vector<std::string> split(const char *str, const char c = ' ')
+  {
+    std::vector<std::string> result;
+
+    if ( str ) 
+    {
+      do
+      {
+        const char *begin = str;
+
+        while(*str != c && *str)
+          str++;
+
+        if ( begin != str )
+          result.push_back(std::string(begin, str));
+      } while (0 != *str++);
+    }
+
+    return result;
+  }
+
+  std::vector<std::string> split(const mg_str str, const char c = ' ')
+  {
+    std::string tmp(str.p, str.len);
+
+    return split( tmp.c_str(), c);
+  }
+
+  std::string get_until_char(std::string const& s, char c)
+  {
+    std::string::size_type pos = s.find(c);
+    if (pos != std::string::npos)
+    {
+      return s.substr(0, pos);
+    }
+    else
+    {
+      return s;
+    }
+  }
+
+};
