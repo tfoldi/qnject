2e41be2082cba99a6192f70b9f9a7a0a89d44fda
Tamas Foldi <tfoldi@starschema.net>
2016-12-28 22:52:02 +0100

    add some comment + try a separate qt repo for travis

--- vaccine/tests/unit_echo.cpp
+++ vaccine/tests/unit_echo.cpp
@@ -9,9 +9,9 @@
 TEST_CASE("Accessing echo thru web service", "[echo]") {
   static const char * s_url = "http://127.0.0.1:8000/api/echo";
 
+  // make sure we're running
   for (auto i = 0; vaccine::state != vaccine::mg_state::RUNNING || i < 10 ; i++)
     usleep(1000);
-
 
   SECTION("Simple http invocation to test respone body") {
    
@@ -22,5 +22,9 @@ TEST_CASE("Accessing echo thru web service", "[echo]") {
 
       });
 
+  }
+
+  SECTION("echo is registred as callback") {
+    CHECK( vaccine::registered_handlers().find( "echo" ) != vaccine::registered_handlers().end() );
   }
 }
--- vaccine/vaccine.cpp
+++ vaccine/vaccine.cpp
@@ -14,23 +14,33 @@ namespace vaccine {
   // vaccine's state. read and write carefully 
   mg_state state = UNINITALIZED;
 
+  // default port, can be override by VACCINE_HTTP_PORT env variable 
   static const char *s_default_http_port = "8000";
+
+  // options for our http server. 
+  // XXX: this could be local var I guess using user_data
   static struct mg_serve_http_opts s_http_server_opts;
 
+  // map handler with their callbacks
   static std::map<std::string, t_vaccine_api_handler> s_uri_handlers;
 
+  // simply push the callback function into our big map of handlers
+  // this function is called from our handler "plugins" 
   void register_callback(std::string handler, t_vaccine_api_handler function) {
     s_uri_handlers[handler] = function;
   }
 
+  // const getter for the registred handlers
   const std::map<std::string, t_vaccine_api_handler> registered_handlers() { 
     return s_uri_handlers;
   };
 
+  // check if an mg_str starts with an another mg_str. 
   static int has_prefix(const struct mg_str *uri, const struct mg_str *prefix) {
     return uri->len > prefix->len && memcmp(uri->p, prefix->p, prefix->len) == 0;
   }
 
+  // get parsed json object from a http PUT/POST data req
   void parse_request_body(struct http_message *hm, nlohmann::json & req)
   {
     if ( hm->body.len > 0 ) {
@@ -44,7 +54,7 @@ namespace vaccine {
     }
   }
 
-
+  // send json response to the client
   void send_json(struct mg_connection *nc, nlohmann::json & j, int statusCode) {
     std::string d = j.dump(2);
 
@@ -52,6 +62,10 @@ namespace vaccine {
     mg_send(nc,d.c_str(),d.length());
   }
 
+  // this is our big fat HTTP event handler
+  //
+  // if URI starts with /api then it calls the matching handler
+  // else tries to serve the static content piece
   static void ev_handler(struct mg_connection *nc, int ev, void *ev_data) {
     struct http_message *hm = (struct http_message *) ev_data;
     static const struct mg_str api_prefix = MG_MK_STR("/api/");
@@ -87,11 +101,10 @@ namespace vaccine {
     }
   }
 
-
+  // simply start our HTTP server thread
   void start_thread() {
     struct mg_mgr mgr;
     struct mg_connection *nc;
-    int i;
     const char * http_port = getenv("VACCINE_HTTP_PORT") == NULL ? s_default_http_port : getenv("VACCINE_HTTP_PORT");
 
     /* Open listening socket */
@@ -102,10 +115,11 @@ namespace vaccine {
     /* mount UI. TODO: search for the right path */
     s_http_server_opts.document_root = "./elm-street"; 
 
-    /* Run event loop until signal is received */
+    /* socket bind, document root set, ready to serve */
     state = mg_state::RUNNING;
     printf("Starting vaccine HTTP REST API server on port %s\n", http_port);
 
+    /* Run event loop until signal is received */
     while (state == mg_state::RUNNING) {
       mg_mgr_poll(&mgr, 1000);
     }
@@ -113,8 +127,6 @@ namespace vaccine {
     /* Cleanup */
     mg_mgr_free(&mgr);
     state = mg_state::NOT_RUNNING;
-
-    printf("Exiting on shutdown request");
   }
 
 }
