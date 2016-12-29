e5a962282e5a1efb0d97684ce55966d00933d116
Tamas Foldi <tfoldi@starschema.net>
2016-12-29 16:46:57 +0100

    add working directory to xdd

--- default.nix
+++ default.nix
@@ -3,17 +3,25 @@
   qnject = pkgs.stdenv.mkDerivation rec {
     name = "qnject";
     src = ./.;
+    
     buildInputs = with pkgs; [ cmake vim ];
+
+    dontUseCmakeConfigure = true;
+    dontUseCmakeBuildDir = true;
+
+  
     cmakeFlags = (if ! use_clang then "" else ''
       -DCMAKE_C_COMPILER=${pkgs.clang}/bin/clang
       -DCMAKE_CXX_COMPILER=${pkgs.clang}/bin/clang++
     '') + (if ! use_qt then "" else ''
       -DCMAKE_PREFIX_PATH=${pkgs.qt5.full}
     '');
-    preBuild = "ln -s build/config.h ../config.h";
+
+    buildPhase = ''
+        export TARGET="$out"
+      '';
+
     installPhase = ''
-      mkdir -p $out/lib
-      cp vaccine/libqnject.so $out/lib/
       ctest -V
     '';
   };
